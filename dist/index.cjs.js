de51ad51990a8e45ea24a919b44b1b239b9d7aab
PonySwapFinance <ponyswappns@protonmail.com>
2023-03-15 07:12:47 +0900

    Twitter

--- dist/index.cjs.js
+++ dist/index.cjs.js
@@ -2276,62 +2276,14 @@ var socials = [
         items: [
             {
                 label: "English",
-                href: "https://t.me/goosedefi",
-            },
-            // {
-            //   label: "Bahasa Indonesia",
-            //   href: "https://t.me/PancakeSwapIndonesia",
-            // },
-            {
-                label: "中文",
-                href: "https://t.me/goosefinancechinese",
-            },
-            // {
-            //   label: "Tiếng Việt",
-            //   href: "https://t.me/PancakeSwapVN",
-            // },
-            {
-                label: "Italiano",
-                href: "https://t.me/goosefinanceitalian",
-            },
-            {
-                label: "русский",
-                href: "https://t.me/goosefinancerussian",
-            },
-            {
-                label: "Türkiye",
-                href: "https://t.me/GooseFinanceTurkey",
-            },
-            // {
-            //   label: "Português",
-            //   href: "https://t.me/PancakeSwapPortuguese",
-            // },
-            {
-                label: "Español",
-                href: "https://t.me/goosefinancespanish",
-            },
-            {
-                label: "日本語",
-                href: "https://t.me/goosefinancejapanese",
-            },
-            {
-                label: "Français",
-                href: "https://t.me/goosefinancefrench",
-            },
-            {
-                label: "Announcements",
-                href: "https://t.me/goosefinanceann",
-            },
-            {
-                label: "Price Bot",
-                href: "https://t.me/BinanceRocketEGG",
+                href: "https://t.me/ponyswapfinance",
             },
         ],
     },
     {
         label: "Twitter",
         icon: "TwitterIcon",
-        href: "https://twitter.com/ponyswapfinance",
+        href: "https://twitter.com/PonySwapFinance",
     },
 ];
 var MENU_HEIGHT = 64;
--- src/widgets/Menu/config.ts
+++ src/widgets/Menu/config.ts
@@ -120,62 +120,62 @@ export const socials = [
     items: [
       {
         label: "English",
-        href: "https://t.me/goosedefi",
+        href: "https://t.me/ponyswapfinance",
       },
       // {
       //   label: "Bahasa Indonesia",
       //   href: "https://t.me/PancakeSwapIndonesia",
       // },
-      {
-        label: "中文",
-        href: "https://t.me/goosefinancechinese",
-      },
+      // {
+      //   label: "中文",
+      //   href: "https://t.me/goosefinancechinese",
+      // },
       // {
       //   label: "Tiếng Việt",
       //   href: "https://t.me/PancakeSwapVN",
       // },
-      {
-        label: "Italiano",
-        href: "https://t.me/goosefinanceitalian",
-      },
-      {
-        label: "русский",
-        href: "https://t.me/goosefinancerussian",
-      },
-      {
-        label: "Türkiye",
-        href: "https://t.me/GooseFinanceTurkey",
-      },
+      // {
+      //   label: "Italiano",
+      //   href: "https://t.me/goosefinanceitalian",
+      // },
+      // {
+      //   label: "русский",
+      //   href: "https://t.me/goosefinancerussian",
+      // },
+      // {
+      //   label: "Türkiye",
+      //   href: "https://t.me/GooseFinanceTurkey",
+      // },
       // {
       //   label: "Português",
       //   href: "https://t.me/PancakeSwapPortuguese",
       // },
-      {
-        label: "Español",
-        href: "https://t.me/goosefinancespanish",
-      },
-      {
-        label: "日本語",
-        href: "https://t.me/goosefinancejapanese",
-      },
-      {
-        label: "Français",
-        href: "https://t.me/goosefinancefrench",
-      },
-      {
-        label: "Announcements",
-        href: "https://t.me/goosefinanceann",
-      },
-      {
-        label: "Price Bot",
-        href: "https://t.me/BinanceRocketEGG",
-      },
+      // {
+      //   label: "Español",
+      //   href: "https://t.me/goosefinancespanish",
+      // },
+      // {
+      //   label: "日本語",
+      //   href: "https://t.me/goosefinancejapanese",
+      // },
+      // {
+      //   label: "Français",
+      //   href: "https://t.me/goosefinancefrench",
+      // },
+      // {
+      //   label: "Announcements",
+      //   href: "https://t.me/goosefinanceann",
+      // },
+      // {
+      //   label: "Price Bot",
+      //   href: "https://t.me/BinanceRocketEGG",
+      // },
     ],
   },
   {
     label: "Twitter",
     icon: "TwitterIcon",
-    href: "https://twitter.com/ponyswapfinance",
+    href: "https://twitter.com/PonySwapFinance",
   },
 ];
 

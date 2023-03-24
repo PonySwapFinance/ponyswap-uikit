daeb3bf6a034a819af8f4a27fb4455088cbad3b5
PonySwapFinance <ponyswappns@protonmail.com>
2023-03-24 15:32:05 +0900

    Mobile Menu

--- src/widgets/Menu/Panel.tsx
+++ src/widgets/Menu/Panel.tsx
@@ -34,7 +34,7 @@ const StyledPanel = styled.div<{ isPushed: boolean; showMenu: boolean }>`
     width: ${({ isPushed }) => `${isPushed ? SIDEBAR_WIDTH_FULL : SIDEBAR_WIDTH_REDUCED}px`};
   }
 
-  background-color: rgba(0,0,0,0.25) !important;
+  background-color: rgba(0,0,0,0.25);
   border: none !important;
 
   top: 64px;
